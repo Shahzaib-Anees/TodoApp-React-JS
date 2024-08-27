88b3273af0d9a6800769e9b9aec431c6d927f853
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-08-27 15:53:21 +0500

    Adding Styling to the Application

--- src/App.jsx
+++ src/App.jsx
@@ -27,33 +27,42 @@ function TodoApp() {
 
   return (
     <>
-      <form onSubmit={createTodo}>
+      <h1>Todo Application</h1>
+      <form onSubmit={createTodo} id="todo-input-form">
         <input type="text" placeholder="enter task" ref={todoTask} />
         <button type="submit">Set Task</button>
       </form>
-      <ol>
-        {todoArr.map((task, index) => {
-          return (
-            <div key={index}>
-              <li>{task}</li>
-              <button
-                onClick={() => {
-                  deleteTodoTask(index);
-                }}
-              >
-                Delete Todo
-              </button>
-              <button
-                onClick={() => {
-                  editTodoTask(index);
-                }}
-              >
-                Edit Todo
-              </button>
-            </div>
-          );
-        })}
-      </ol>
+      <div id="todo-list-dom">
+        {todoArr.length > 0 ? (
+          todoArr.map((task, index) => {
+            return (
+              <div key={index} className="todo-list-task">
+                <p>{task}</p>
+                <div id="button-cont">
+                  <button
+                    className="task-button"
+                    onClick={() => {
+                      deleteTodoTask(index);
+                    }}
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                  <button
+                    className="task-button"
+                    onClick={() => {
+                      editTodoTask(index);
+                    }}
+                  >
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </button>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <p id="elsed-para">No Task Found !</p>
+        )}
+      </div>
     </>
   );
 }
